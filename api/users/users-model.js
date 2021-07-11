457f5dcd023e8d0a31b0a6efe1c9250cc8e0fd73
Gabriel Henton <gabehenton@gmail.com>
2021-07-11 15:16:00 -0700

    users-model done

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -18,6 +18,12 @@ function find() {
       }
     ]
    */
+
+
+  return db("users as u")
+    .join("roles as r", "u.role_id", "=", "r.role_id")
+    .select("u.role_id", "u.username", "r.role_name as role");
+
 }
 
 function findBy(filter) {
@@ -34,6 +40,10 @@ function findBy(filter) {
       }
     ]
    */
+  return db("users as u")
+    .join("roles as r", "u.role_id", "=", "r.role_id")
+    .select("u.role_id", "u.username", "r.role_name as role", "u.password")
+    .where(filter);
 }
 
 function findById(user_id) {
@@ -47,6 +57,11 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+  return db("users as u")
+    .join("roles as r", "u.role_id", "=", "r.role_id")
+    .select("u.role_id", "u.username", "r.role_name as role")
+    .where("u.role_id", user_id)
+    .first();
 }
 
 /**
@@ -90,3 +105,4 @@ module.exports = {
   findBy,
   findById,
 };
+
